cee5ec89b90cedb812c50be7a13ec8177da579f2
Thanh Tran <tt222kj@student.lnu.se>
2021-05-10 21:36:23 +0200

    try again

--- src/server.js
+++ src/server.js
@@ -42,10 +42,10 @@ const main = async () => {
 
   if (process.env.NODE_ENV === 'production') {
     // Serve static files.
-    app.use(express.static(__dirname + '/dist'))
+    app.use(express.static('..' + 'client/dist'))
 
     app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname + '/dist/index.html'))
+      res.sendFile(path.resolve('..' + 'client/dist/index.html'))
     })
   }
 
